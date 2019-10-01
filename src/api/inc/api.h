655838a0b0beea0f2eacee2026c42789cf342e41
John Hurliman <jhurliman@jhurliman.org>
2019-09-30 22:52:21 -0700

    Pass args with a struct, OpenCL API support

--- src/api/inc/api.h
+++ src/api/inc/api.h
@@ -2,9 +2,68 @@
 
 #include <stdint.h>
 
-extern "C" __declspec(dllexport) bool VHACDConvexDecomposition(
-	const double* inPoints, uint32_t inNumPoints, const int32_t* inTriangles, uint32_t inNumTriangles,
-	double** outPoints, uint32_t* outNumPoints, int32_t** outTriangles, uint32_t* outNumTriangles,
-	int32_t** outPointOffsets, int32_t** outTriangleOffsets, uint32_t* outNumMeshes);
+#pragma pack(push, 8)
+struct VHACDSession {
+	// Maximum number of convex hulls to produce (default = 64, range = 1 - 1024)
+	int32_t maxConvexHulls = 64;
+	// Maximum number of voxels generated during the voxelization stage
+	// (default = 100,000, range = 10,000 - 64,000,000)
+	int32_t resolution = 100000;
+	// Maximum allowed concavity (default = 0.0025, range = 0.0 - 1.0)
+	double concavity = 0.0025;
+	// Controls the granularity of the search for the "best" clipping plane
+	// (default = 4, range = 1 - 16)
+	int32_t planeDownsampling = 4;
+	// Controls the precision of the convex - hull generation process during
+	// the clipping plane selection stage (default = 4, range = 1 - 16)
+	int32_t convexhullDownsampling = 4;
+	// Controls the bias toward clipping along symmetry planes
+	// (default = 0.05, range = 0.0 - 1.0)
+	double alpha = 0.05;
+	// Controls the bias toward clipping along revolution axes
+	// (default = 0.05, range = 0.0 - 1.0)
+	double beta = 0.05;
+	// Enable / disable normalizing the mesh before applying the convex
+	// decomposition (default = 0, range = { 0,1 })
+	int32_t pca = 0;
+	// 0: voxel - based approximate convex decomposition, 1 : tetrahedron - based
+	// approximate convex decomposition(default = 0, range = { 0,1 })
+	int32_t mode = 0;
+	// Controls the maximum number of triangles per convex hull
+	// (default = 64, range = 4 - 1024)
+	int32_t maxNumVerticesPerCH = 64;
+	// Controls the adaptive sampling of the generated convex hulls
+	// (default = 0.0001, range = 0.0 - 0.01)
+	double minVolumePerCH = 0.0001;
+	// Enable / disable approximation when computing convex hulls
+	// (default = true)
+	int32_t convexHullApproximation = 1;
+	// Project the output convex hull vertices onto the original source mesh to
+	// increase the floating point accuracy of the results (default = true)
+	int32_t projectHullVertices = 1;
+	// Enable / disable OpenCL acceleration (default = false)
+	int32_t oclAcceleration = 0;
+	// OpenCL platform id (default = 0, range = 0 - # OCL platforms)
+	int32_t oclPlatformID = 0;
+	// OpenCL device id (default = 0, range = 0 - # OCL devices)
+	int32_t oclDeviceID = 0;
 
-extern "C" __declspec(dllexport) void VHACDDestroy(double** points, int32_t** triangles, int32_t** pointOffsets, int32_t** triangleOffsets);
+	double* outPoints;
+	int32_t* outTriangles;
+	int32_t* outPointOffsets;
+	int32_t* outTriangleOffsets;
+	int32_t outNumPoints;
+	int32_t outNumTriangles;
+	int32_t outNumMeshes;
+};
+#pragma pack(pop)
+
+extern "C" __declspec(dllexport) bool VHACDGetOpenCLPlatforms(char outPlatforms[8][64]);
+
+extern "C" __declspec(dllexport) bool VHACDGetOpenCLDevices(uint32_t platformIndex, char outDevices[8][64]);
+
+extern "C" __declspec(dllexport) bool VHACDConvexDecomposition(
+	const double& inPoints, int32_t inNumPoints, const int32_t& inTriangles,
+	int32_t inNumTriangles, VHACDSession& session);
+
+extern "C" __declspec(dllexport) void VHACDShutdown(VHACDSession& session);
--- src/api/src/api.cpp
+++ src/api/src/api.cpp
@@ -1,46 +1,125 @@
 #include "api.h"
 #include "VHACD.h"
+#include "oclHelper.h"
 
 #include <algorithm>
 #include <vector>
 
+extern "C" bool VHACDGetOpenCLPlatforms(char outPlatforms[8][64]) {
+	// Clear all existing platform names
+	memset(outPlatforms, 0, 8 * 64);
+
+	// Query for the number of platforms
+	cl_uint numPlatforms;
+	cl_int lastError = clGetPlatformIDs(1, nullptr, &numPlatforms);
+	if (lastError != CL_SUCCESS)
+		return false;
+
+	// Query for platformIDs
+	cl_platform_id* platforms = new cl_platform_id[numPlatforms];
+	lastError = clGetPlatformIDs(numPlatforms, platforms, nullptr);
+	if (lastError != CL_SUCCESS) {
+		delete[] platforms;
+		return false;
+	}
+
+	// Iterate over each platformID and query the platform vendor name
+	for (cl_uint i = 0; i < numPlatforms && i < 8; ++i) {
+		lastError = clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 64, outPlatforms[i], nullptr);
+		if (lastError != CL_SUCCESS) {
+			delete[] platforms;
+			return false;
+		}
+	}
+
+	delete[] platforms;
+	return true;
+}
+
+extern "C" bool VHACDGetOpenCLDevices(uint32_t platformIndex, char outDevices[8][64]) {
+	cl_uint numPlatforms;
+	cl_int lastError = clGetPlatformIDs(1, nullptr, &numPlatforms);
+	if (lastError != CL_SUCCESS || platformIndex >= numPlatforms) {
+		return false;
+	}
+
+	cl_platform_id* platforms = new cl_platform_id[numPlatforms];
+	lastError = clGetPlatformIDs(numPlatforms, platforms, nullptr);
+	if (lastError != CL_SUCCESS) {
+		delete[] platforms;
+		return false;
+	}
+
+	cl_platform_id platform = platforms[platformIndex];
+
+	// Clear all existing device names
+	memset(outDevices, 0, 8 * 64);
+
+	// Query for the number of devices
+	cl_uint numDevices;
+	lastError = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &numDevices);
+	if (lastError != CL_SUCCESS) {
+		return false;
+	}
+
+	// Query for deviceIDs
+	cl_device_id* devices = new cl_device_id[numDevices];
+	lastError = clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, numDevices, devices, nullptr);
+	if (lastError != CL_SUCCESS) {
+		delete[] devices;
+		return false;
+	}
+
+	// Iterate over each deviceID and query the device name
+	for (cl_uint i = 0; i < numDevices && i < 8; ++i) {
+		lastError = clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 64, outDevices[i], nullptr);
+		if (lastError != CL_SUCCESS) {
+			delete[] devices;
+			return false;
+		}
+	}
+
+	delete[] devices;
+	return true;
+}
+
 extern "C" bool VHACDConvexDecomposition(
-	const double* inPoints, uint32_t inNumPoints, const int32_t* inTriangles, uint32_t inNumTriangles,
-	double** outPoints, uint32_t* outNumPoints, int32_t** outTriangles, uint32_t* outNumTriangles,
-	int32_t** outPointOffsets, int32_t** outTriangleOffsets, uint32_t* outNumMeshes) {
+	const double& inPoints, int32_t inNumPoints, const int32_t& inTriangles,
+	int32_t inNumTriangles, VHACDSession& session) {
 
 	VHACD::IVHACD::Parameters params;
-	/*params.m_alpha;
-	params.m_beta;
-	params.m_callback;
-	params.m_concavity;
-	params.m_convexhullApproximation;
-	params.m_convexhullDownsampling;
-	params.m_maxConvexHulls;
-	params.m_maxNumVerticesPerCH;
-	params.m_minVolumePerCH;
-	params.m_mode;*/
-	params.m_oclAcceleration = false;
-	/*params.m_pca;
-	params.m_planeDownsampling;
-	params.m_projectHullVertices;
-	params.m_resolution;*/
+	params.m_alpha = session.alpha;
+	params.m_beta = session.beta;
+	//params.m_callback;
+	params.m_concavity = session.concavity;
+	params.m_convexhullApproximation = uint32_t(session.convexHullApproximation);
+	params.m_convexhullDownsampling = uint32_t(session.convexhullDownsampling);
+	params.m_maxConvexHulls = uint32_t(session.maxConvexHulls);
+	params.m_maxNumVerticesPerCH = uint32_t(session.maxNumVerticesPerCH);
+	params.m_minVolumePerCH = session.minVolumePerCH;
+	params.m_mode = uint32_t(session.mode);
+	params.m_oclAcceleration = uint32_t(session.oclAcceleration);
+	params.m_pca = uint32_t(session.pca);
+	params.m_planeDownsampling = uint32_t(session.planeDownsampling);
+	params.m_projectHullVertices = session.projectHullVertices;
+	params.m_resolution = session.resolution;
 
 	VHACD::IVHACD* interfaceVHACD = VHACD::CreateVHACD();
 
-	//bool res = interfaceVHACD->OCLInit(oclHelper.GetDevice(), &myLogger);
+	OCLHelper oclHelper;
+	if (session.oclAcceleration)
+	{
+		oclHelper.InitPlatform(session.oclPlatformID);
+		oclHelper.InitDevice(session.oclDeviceID);
+		if (!interfaceVHACD->OCLInit(oclHelper.GetDevice()))
+		{
+			params.m_oclAcceleration = 0;
+		}
+	}
 
 	// Compute convex hulls
-	bool res = interfaceVHACD->Compute(inPoints, inNumPoints, reinterpret_cast<const uint32_t*>(inTriangles), inNumTriangles, params);
-
-	if (!res) {
-		*outPoints = nullptr;
-		*outTriangles = nullptr;
-		*outPointOffsets = nullptr;
-		*outTriangleOffsets = nullptr;
-		*outNumPoints = 0;
-		*outNumTriangles = 0;
-		*outNumMeshes = 0;
+	const uint32_t& inUTriangles = reinterpret_cast<const uint32_t&>(inTriangles);
+	if (!interfaceVHACD->Compute(&inPoints, uint32_t(inNumPoints), &inUTriangles, uint32_t(inNumTriangles), params)) {
 		return false;
 	}
 
@@ -59,23 +138,23 @@ extern "C" bool VHACDConvexDecomposition(
 	}
 
 	// Put all the geometry data into contiguous output arrays
-	*outPoints = new double[numPoints * 3];
-	*outTriangles = new int32_t[numTriangles * 3];
-	*outPointOffsets = new int32_t[numConvexHulls];
-	*outTriangleOffsets = new int32_t[numConvexHulls];
-	*outNumPoints = numPoints;
-	*outNumTriangles = numTriangles;
-	*outNumMeshes = numConvexHulls;
+	session.outPoints = new double[numPoints * 3];
+	session.outTriangles = new int32_t[numTriangles * 3];
+	session.outPointOffsets = new int32_t[numConvexHulls];
+	session.outTriangleOffsets = new int32_t[numConvexHulls];
+	session.outNumPoints = numPoints;
+	session.outNumTriangles = numTriangles;
+	session.outNumMeshes = numConvexHulls;
 	int32_t pointOffset = 0;
 	int32_t triangleOffset = 0;
 	for (uint32_t i = 0; i < numConvexHulls; ++i) {
 		VHACD::IVHACD::ConvexHull& convexHull = convexHulls[i];
 
-		std::copy(convexHull.m_points, convexHull.m_points + convexHull.m_nPoints * 3, (*outPoints) + pointOffset);
-		std::copy(convexHull.m_triangles, convexHull.m_triangles + convexHull.m_nTriangles * 3, (*outTriangles) + triangleOffset);
+		std::copy(convexHull.m_points, convexHull.m_points + convexHull.m_nPoints * 3, session.outPoints + pointOffset);
+		std::copy(convexHull.m_triangles, convexHull.m_triangles + convexHull.m_nTriangles * 3, session.outTriangles + triangleOffset);
 
-		(*outPointOffsets)[i] = pointOffset;
-		(*outTriangleOffsets)[i] = triangleOffset;
+		session.outPointOffsets[i] = pointOffset;
+		session.outTriangleOffsets[i] = triangleOffset;
 
 		pointOffset += convexHull.m_nPoints * 3;
 		triangleOffset += convexHull.m_nTriangles * 3;
@@ -84,14 +163,14 @@ extern "C" bool VHACDConvexDecomposition(
 	return true;
 }
 
-extern "C" void VHACDDestroy(double** points, int32_t** triangles, int32_t** pointOffsets, int32_t** triangleOffsets) {
-	delete (*points);
-	delete (*triangles);
-	delete (*pointOffsets);
-	delete (*triangleOffsets);
+extern "C" void VHACDShutdown(VHACDSession& session) {
+	delete[] session.outPoints;
+	delete[] session.outTriangles;
+	delete[] session.outPointOffsets;
+	delete[] session.outTriangleOffsets;
 
-	*points = nullptr;
-	*triangles = nullptr;
-	*pointOffsets = nullptr;
-	*triangleOffsets = nullptr;
+	session.outPoints = nullptr;
+	session.outTriangles = nullptr;
+	session.outPointOffsets = nullptr;
+	session.outTriangleOffsets = nullptr;
 }
